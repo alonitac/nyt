4843356d006107a0c4b188ef7d89698b883223ff
Alon Itach <alon.itach@mail.huji.ac.il>
2019-02-16 23:50:45 +0200

    finish importing data. start build visualization tool

--- api_client.py
+++ api_client.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import pymongo
 import requests
+from pymongo.errors import DuplicateKeyError
+import json
+import dateutil.parser
 
 
 class APIclient(object):
@@ -21,14 +24,35 @@ class APIclient(object):
     def push_archive(self, year, month):
         url = self.base_url.format(year, month, self.key)
         data = requests.get(url).json()
+        summary = {
+            'inserted': [],
+            'duplication': []
+        }
         for item in data['response']['docs']:
-            r = self.dbclient[self.db_name][str(year)][str(month)].insert_one(item).inserted_id
-            print(r)
+            try:
+                r = self.dbclient[self.db_name][str(year)][str(month)].insert_one(item).inserted_id
+                summary['inserted'].append(r)
+            except DuplicateKeyError as e:
+                summary['duplication'].append(str(e)[85: -3])
+
+        return summary
 
     def fetch_archive(self):
-        pass
+        self.dbclient[self.db_name].find()
+
+    def get_data_from_api(self, y_from, y_to, m_from=1, m_to=12):
+        years = range(y_from, y_to+1)
+        months = range(m_from, m_to+1)
+        for y in years:
+            for m in months:
+                summary = api_client.push_archive(y, m)
+                with open('data/duplications/{}_{}.json'.format(y, m), 'w') as fp:
+                    json.dump(summary, fp)
+
+    def drop_collection(self, col):
+        res = self.dbclient[self.db_name][col].drop()
+        print(res)
 
 
 if __name__ == '__main__':
     api_client = APIclient()
-    api_client.push_archive(2019, 1)
